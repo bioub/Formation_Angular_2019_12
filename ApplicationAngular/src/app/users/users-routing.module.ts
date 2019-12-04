bfaa03cbde47ddc430249d6825750657a379d1d1
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2019-12-04 13:48:12 +0100

    Routes

--- ApplicationAngular/src/app/users/users-routing.module.ts
+++ ApplicationAngular/src/app/users/users-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+/*
+Exercice :
+Créer et router 3 composants dans les modules User et UserRouting :
+UsersComponent -> URL /users
+UserDetailsComponent -> URL /users/details
+UserAddComponent -> URL /users/add
 
+Importer UserModule depuis AppModule pour activer les routes
+*/
 const routes: Routes = [];
 
 @NgModule({
